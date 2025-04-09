f9ced0fa9e79904045472ce394a3f0bc75dd6069
Divyanshu Sharma <sharmadivyanshu265@gmail.com>
2025-04-09 10:12:24 +0530

    tc

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+// filepath: c:\Users\sharm\Downloads\project\webpack.config.js
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Customize the config here if needed
+  return config;
+};
